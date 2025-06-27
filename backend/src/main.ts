8dc24a08942892400628b800bd514e3f95405e08
Samuel Aquino <samuelaquinoandujar@gmail.com>
2025-06-26 21:18:54 -0400

    refactor: prefix api removed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 8080);
 }
