ac464477b1ea9d1c689056b684f2355244040e95
Samuel Aquino <samuelaquinoandujar@gmail.com>
2025-06-26 20:56:53 -0400

    refactor: listen port 8080

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -36,6 +36,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3012);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
